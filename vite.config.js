8e9d0a01b64b76b8a84f543e6771db47a66d1de0
jsteinfink914 <jsteinfink914@gmail.com>
2025-02-16 17:57:52 -0500

    adding type field to package

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  publicDir: 'public'
+  root: 'src',  // Adjust this if your source files are in a different folder
+  build: {
+    rollupOptions: {
+      input: '/public/index.html' // Ensure this points to the correct location
+    }
+  },
+  publicDir: 'public', // This should be the default, but just to be sure
 });
