32614f146b05620ece417ce8ee12284156c972ed
jsteinfink914 <jsteinfink914@gmail.com>
2025-02-16 17:10:27 -0500

    Updating svelte

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import svelte from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: 'build', // The folder where Vercel will expect the production-ready files
-  },
+  plugins: [svelte()]
 });
