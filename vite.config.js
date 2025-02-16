bdd885962397d9e8e837faa0ab5714e8066fe859
jsteinfink914 <jsteinfink914@gmail.com>
2025-02-16 18:28:22 -0500

    adding funcitonality

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite';
-
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-
-
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  root: '.',  // Assuming the project root contains the public folder
+  root: '.',  // The root is the project folder, assuming 'index.html' is there
   build: {
     rollupOptions: {
-      input: '/public/index.html',  // Explicit path to index.html
+      input: 'public/index.html',  // Relative path to index.html from the root
     },
+    outDir: 'dist',  // Output directory for the build files
   },
   publicDir: 'public',  // Ensure Vite knows to serve files from the public folder
 });
