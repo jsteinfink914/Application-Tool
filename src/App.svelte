a8736008c6a0277260354a947f355d3c7de19137
jsteinfink914 <jsteinfink914@gmail.com>
2025-02-25 20:02:10 -0500

    Adding split view

--- src/App.svelte
+++ src/App.svelte
@@ -947,7 +947,6 @@ function handleScroll() {
     {:else}
         <p>Listings Loading...</p>
     {/if}
-  </div>
 {:else}
   <div id="container">
     <button class="sidebar-toggle" class:open={sidebarOpen} on:click={toggleSidebar}>
