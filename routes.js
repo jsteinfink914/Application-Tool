886953d306225691336113479cf0fff836f5427b
jsteinfink914 <jsteinfink914@gmail.com>
2025-02-27 20:06:50 -0500

    Adding routes

--- routes.js
+++ routes.js
@@ -1,7 +1,6 @@
 import BuildingDashboard from "./routes/BuildingDashboard.svelte";
 import RenterPortal from "./routes/RenterPortal.svelte";
 import MovingServices from "./routes/MovingServices.svelte";
-import RenterPortal from "./routes/renter-portal.svelte";
 
 const routes = {
   "/building-dashboard": BuildingDashboard,
