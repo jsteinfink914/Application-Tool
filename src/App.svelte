cd4cc5011a54e6662a2e3040066122776dbd80f3
jsteinfink914 <jsteinfink914@gmail.com>
2025-02-28 17:12:19 -0500

    Adding more listings

--- src/App.svelte
+++ src/App.svelte
@@ -943,6 +943,7 @@ async function applyFilters() {
 
 .content-container {
     padding-top: 40px; /* Pushes content below the banner */
+    background-color: #FBF7F0;
 }
 
 .view-toggle-container {
